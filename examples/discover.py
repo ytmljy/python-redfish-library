cc42cf7c9f2eaef564c07a99c3edabbd48fd6f3e
DESKTOP-I1CD95E\ytmlj <ytmljy@gmail.com>
2023-04-21 18:03:54 +0900

    print(services)

--- examples/discover.py
+++ examples/discover.py
@@ -9,6 +9,7 @@ import redfish
 
 print('call main before')
 services = redfish.discover_ssdp(port=1900, ttl=10, response_time=10, iface=None, protocol="ipv4", address=None)
+print(services)
 print('call main after')
 for service in services:
     print( '{}: {}'.format(service, services[service]))
